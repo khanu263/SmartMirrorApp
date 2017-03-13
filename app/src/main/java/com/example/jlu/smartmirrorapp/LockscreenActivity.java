6dd280557a8daf5109bee22072eee3c82460555c
salsj345 <joshuasalsbury@gmail.com>
2017-03-13 10:25:05 -0700

    Finished Lockscreen activity

--- app/src/main/java/com/example/jlu/smartmirrorapp/LockscreenActivity.java
+++ app/src/main/java/com/example/jlu/smartmirrorapp/LockscreenActivity.java
@@ -1,14 +1,22 @@
 package com.example.jlu.smartmirrorapp;
 
 import android.annotation.SuppressLint;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -93,15 +101,88 @@ public class LockscreenActivity extends AppCompatActivity {
         // Returns string "am" or "pm"
         String am_pm;
 
-        if (hourOfDay > 12) {
-            am_pm = "pm";
+        if (hourOfDay >= 12) {
+            am_pm = getResources().getString(R.string.period_pm);
         }
 
         else {
-            am_pm = "am";
+            am_pm = getResources().getString(R.string.period_am);
         }
 
         return am_pm;
+
+    }
+
+    public String getGreeting(int hourOfDay) {
+        // Returns string "morning", "afternoon", or "evening"
+        String greeting;
+
+        if (hourOfDay >= 4 && hourOfDay < 12) {
+            greeting = getResources().getString(R.string.greeting_morning);
+        }
+
+        else if (hourOfDay >= 12 && hourOfDay < 17) {
+            greeting = getResources().getString(R.string.greeting_afternoon);
+        }
+
+        else {
+            greeting = getResources().getString(R.string.greeting_evening);
+        }
+
+        return greeting;
+
+    }
+
+    public String getDateString() {
+        // Returns date string
+        Calendar sCalendar = Calendar.getInstance();
+        String dayOfWeek = sCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+
+        SimpleDateFormat month_name = new SimpleDateFormat("MMMM");
+        String monthName = month_name.format(sCalendar.getTime());
+
+        SimpleDateFormat month_day = new SimpleDateFormat("dd");
+        String dayOfMonth = month_day.format(sCalendar.getTime());
+
+        String dateString = dayOfWeek + ", " + monthName + " " + dayOfMonth;
+
+        return dateString;
+    }
+
+    public void updateLockscreen(TextView greeting_text, TextView period_text, TextView date_text, TextView time_text) {
+        // updates screen with latest time/date info
+
+        // get specific time of day info
+        int hourOfDayUnformatted = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        int hourOfDayFormatted = 0;
+
+        if (hourOfDayUnformatted == 0) {
+            hourOfDayFormatted = 12;
+        }
+
+        else if (hourOfDayUnformatted > 12) {
+            hourOfDayFormatted = hourOfDayUnformatted - 12;
+        }
+
+        else {
+            hourOfDayFormatted = hourOfDayUnformatted;
+        }
+
+        int minuteOfDay = Calendar.getInstance().get(Calendar.MINUTE);
+        String timeOfDay = Integer.toString(hourOfDayFormatted) + "." + String.format("%02d", minuteOfDay);
+        Log.d("test", Integer.toString(hourOfDayFormatted));
+        Log.d("test", Integer.toString(hourOfDayUnformatted));
+
+        // get general time of day info
+        String am_pm = AMorPM(hourOfDayUnformatted);
+        String greeting = getGreeting(hourOfDayUnformatted);
+        String dateString = getDateString();
+
+        // set text values
+        period_text.setText(am_pm);
+        greeting_text.setText(greeting);
+        date_text.setText(dateString);
+        time_text.setText(timeOfDay);
 
     }
 
@@ -126,8 +207,24 @@ public class LockscreenActivity extends AppCompatActivity {
         });
 
         // ~~~ Josh changes start here ~~~
-        int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-        String am_pm = AMorPM(hourOfDay);
+
+        // initialize text views for the activity
+        final TextView greeting_text = (TextView) findViewById(R.id.greeting_text);
+        final TextView period_text = (TextView) findViewById(R.id.period_text);
+        final TextView date_text = (TextView) findViewById(R.id.date_text);
+        final TextView time_text = (TextView) findViewById(R.id.time_text);
+
+        CountDownTimer timer = new CountDownTimer(1000000000, 60000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateLockscreen(greeting_text, period_text, date_text, time_text);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("noot", "noot");
+            }
+        }.start();
 
     }
 
