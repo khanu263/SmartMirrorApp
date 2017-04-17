81583cc9f2ae7f8ab04b238cf4ffafd38c22aae1
salsj345 <joshuasalsbury@gmail.com>
2017-04-17 10:50:47 -0700

    Commit before merging News Activity

--- app/src/main/java/com/example/jlu/smartmirrorapp/NotificationsActivity.java
+++ app/src/main/java/com/example/jlu/smartmirrorapp/NotificationsActivity.java
@@ -13,11 +13,22 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+// Import Statements for API Call
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class NotificationsActivity extends AppCompatActivity {
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -37,6 +48,7 @@ public class NotificationsActivity extends AppCompatActivity {
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
+
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
@@ -88,13 +100,79 @@ public class NotificationsActivity extends AppCompatActivity {
         }
     };
 
+    // Initialize API Call Variables
+    static final String API_URL = "https://api.darksky.net/forecast/";
+    static final String API_Key = "5a201cad8871292545e0c2dec0ac0c73";
+    static final String latitude = "45.5229";
+    static final String longitude = "-122.9898";
+    static final String Exclusion = "?exclude=minutely,daily,alerts,flags";
+    static final String Units = "?units=auto";
+
+    // API Call
+    class RetrieveWeather extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {}
+
+        protected String doInBackground(Void... urls) {
+
+            // Do some validation here
+            try {
+                // Build URL and open connection
+                URL url = new URL(API_URL + API_Key + "/" + latitude + "," + longitude + Exclusion + Units);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+
+            if(response == null) {
+                notifications_default.setText(ctx.getResources().getString(R.string.notifications_error));
+            }
+
+            else {
+                Log.d("Info", "Received weather");
+
+                 weatherString = response;
+                 weatherArray = processor.parseNewsJSON(, ctx);
+
+                // Log weather variables
+
+            }
+        }
+    }
+
     // initialize application and context
     protected SmartMirrorApp smartMirrorApp;
     Context ctx = this;
 
     // initialize TextViews
+    TextView notifications_date;
+    TextView notifications_time;
 
     // initialize other variables;
+    DataProcessing processor = new DataProcessing();
     CountDownTimer timer;
 
     private void clearReferences() {
@@ -143,9 +221,16 @@ public class NotificationsActivity extends AppCompatActivity {
             }
         });
 
+        // Activity text views
+        notifications_date = (TextView) findViewById(R.id.notifications_date);
+        notifications_time = (TextView) findViewById(R.id.notifications_time);
+
+        // Mirror timer
         timer = new CountDownTimer(300000, 60000) {
             @Override
-            public void onTick(long millisUntilFinished) {}
+            public void onTick(long millisUntilFinished) {
+                processor.updateMinimizedLockscreen(notifications_date, notifications_time, ctx);
+            }
 
             @Override
             public void onFinish() {
